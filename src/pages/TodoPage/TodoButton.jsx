c4c850b3147771c717ce20d8dc5fd46a93cc8672
pantpad <138708449+pantpad@users.noreply.github.com>
2024-02-27 00:01:49 +0100

    Fix typo

--- src/pages/TodoPage/TodoButton.jsx
+++ src/pages/TodoPage/TodoButton.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-export default function TodoButton({ className, children, ...props }) {
+export default function TodoButton({ children, ...props }) {
   return (
     <button
       className="flex aspect-square w-[100px] items-center justify-center rounded-md bg-slate-100"
