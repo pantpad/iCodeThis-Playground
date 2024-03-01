ce9d98e6e6d360a75341149f32ab8cfc47f89f36
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-01 20:43:12 +0100

    Add material icons

--- src/pages/FormDarkTheme/FormDarkTheme.jsx
+++ src/pages/FormDarkTheme/FormDarkTheme.jsx
@@ -1,10 +1,29 @@
 import Inspo from "../../layout/Inspo";
+import PageLayout from "../../layout/PageLayout";
+
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 export default function FormDarkTheme() {
   return (
     <>
       <Inspo href={"https://icodethis.com/submissions/71507"} />
-      <h1>Form Dark theme</h1>
+      <PageLayout
+        className={
+          "h-[97vh] w-[min(35rem,100vw-2rem)] items-center justify-center"
+        }
+      >
+        <section
+          id="bar"
+          className="flex h-16 w-full rounded-lg border border-white/35"
+        >
+          <article>
+            <MoreHorizIcon />
+          </article>
+          <article></article>
+          <article></article>
+          <article></article>
+        </section>
+      </PageLayout>
     </>
   );
 }
