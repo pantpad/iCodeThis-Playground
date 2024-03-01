6047f1c001bdd148611c7b095ede8a006870f5a4
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-01 17:25:34 +0100

    Add FormDarkTheme page

--- src/pages/FormDarkTheme/FormDarkTheme.jsx
+++ src/pages/FormDarkTheme/FormDarkTheme.jsx
@@ -1,6 +1,9 @@
+import Inspo from "../../layout/Inspo";
+
 export default function FormDarkTheme() {
   return (
     <>
+      <Inspo href={"https://icodethis.com/submissions/71507"} />
       <h1>Form Dark theme</h1>
     </>
   );
