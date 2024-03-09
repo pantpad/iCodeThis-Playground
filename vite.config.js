43c841b411af59707f2e04f3ff85a3c47f5ad3d9
pantpad <138708449+pantpad@users.noreply.github.com>
2024-03-09 12:56:44 +0100

    Removed million lint

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import MillionLint from '@million/lint';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-var plugins = [react()];
-plugins.push(MillionLint.vite())
 export default defineConfig({
-  plugins: plugins
+  plugins: [react()],
 });
